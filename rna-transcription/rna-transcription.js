683bb005fc12356875e9d47b5133fa746b199360
Junior Ales <edilson.ales.jr@gmail.com>
2016-11-04 23:37:35 +0000

    converting string to array ES2015 way

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,18 +1,15 @@
 // RNA :: { Char: Char }
 const RNA = { C:'G', G:'C', A:'U', T:'A' };
 
-// getRna :: Char -> RNA 
+// getRna :: Char -> RNA
 const getRna = function(dna) {
   if (RNA[dna]) return RNA[dna];
   throw Error('Invalid DNA strand: ' + dna);
 };
 
-// toArray :: String -> [Char]
-const toArray = Array.from;
-
 export default class {
   // toRna :: String -> String
   toRna(dna) {
-    return toArray(dna).map(getRna).join('');
+    return [...dna].map(getRna).join('');
   }
 }
