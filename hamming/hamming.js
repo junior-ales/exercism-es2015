322b4262d4c27e3f4c4e535d34dca0baefb36349
Junior Ales <edilson.ales.jr@gmail.com>
2016-11-04 21:38:03 +0000

    solving hamming

---
+++ hamming/hamming.js
@@ -0,0 +1,38 @@
+import { compose } from 'ramda';
+
+// validateLength :: [String] -> [String]
+const validateLength = ([x, y]) => {
+  if (x.length === y.length) return [x, y];
+  throw Error('DNA strands must be of equal length.');
+};
+
+// toArray :: String -> [String]
+const toArray = x => Array.from(x);
+
+// toArrays :: [String] -> [[String]]
+const toArrays = ([x, y]) => [toArray(x), toArray(y)];
+
+// toTuples :: [[String]] -> [[Char]]
+const toTuples = ([xs, ys]) => xs.map((x, idx) => [x, ys[idx]]);
+
+// isTupleEqual :: [[Char]] -> [Boolean]
+const isTupleEqual = xs => xs.map(([x, y]) => x === y);
+
+// sumFalsy :: [Boolean] -> Number
+const sumFalsy = xs => xs.reduce((acc, x) => acc + (x ? 0 : 1), 0);
+
+// sumDifferentTuples :: [[Char]] -> Number
+const sumDifferentTuples = compose(sumFalsy, isTupleEqual);
+
+// validTuples :: [String] -> [[Char]]
+const validTuples = compose(toTuples, toArrays, validateLength);
+
+// strandsDiff :: [String] -> Number
+const strandsDiff = compose(sumDifferentTuples, validTuples);
+
+export default class {
+  // compute :: String -> String -> Number
+  compute(...strands) {
+    return strandsDiff(strands);
+  }
+}
