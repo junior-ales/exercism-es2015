4e333f272544bf13b04056fb15538720c3b1f1b3
Junior Ales <edilson.ales.jr@gmail.com>
2016-11-06 01:20:45 +0000

    solving bob

--- bob/bob.js
+++ bob/bob.js
@@ -1,15 +1,35 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+import { both } from 'ramda';
+
+// isUpperCase :: String -> Boolean
+const isUpperCase = c => c === c.toUpperCase();
+
+// isQuestion :: String -> Boolean
+const isQuestion = s => s.endsWith('?');
+
+// hasLetters :: String -> Boolean
+const hasLetters = s => /[a-zA-Z]/.test(s);
+
+// allUpperCase :: String -> Boolean
+const allUpperCase = s => [...s].every(isUpperCase);
+
+// allLettersUppercase :: String -> Boolean
+const allLettersUppercase = both(hasLetters, allUpperCase);
+
+// isEmpty :: String -> Boolean
+const isEmpty = s => s.trim().length === 0;
 
 class Bob {
+  // hey :: String -> String
   hey(message) {
-    //
-    // YOUR CODE GOES HERE
-    //
+    if (isEmpty(message))
+      return 'Fine. Be that way!';
+    else if (allLettersUppercase(message))
+      return 'Whoa, chill out!';
+    else if (isQuestion(message))
+      return 'Sure.';
+    else
+      return 'Whatever.';
   }
 }
 
 export default Bob;
-
