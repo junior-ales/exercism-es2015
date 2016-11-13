73b3cfa8493c55d6102ce24c9669d4d85d8c0723
Junior Ales <edilson.ales.jr@gmail.com>
2016-11-13 15:44:58 +0000

    refactoring pangram and package.json

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -6,14 +6,14 @@ const isEmpty = xs => xs.length === 0;
 // removeChar :: [Char] -> Char -> [Char]
 const removeChar = (cs, c) => cs.filter(_c => _c !== c);
 
+// toArray :: T -> [T]
+const toArray = Array.from;
+
 // alphabet :: [Char]
 const alphabet = toArray('abcdefghijklmnopqrstuvxywz');
 
 // reduceAlphabet :: [Char] -> [Char]
 const reduceAlphabet = reduce(removeChar, alphabet);
-
-// toArray :: T -> [T]
-const toArray = Array.from;
 
 // toLowerCase :: String -> String
 const toLowerCase = s => s.toLowerCase();
@@ -21,11 +21,7 @@ const toLowerCase = s => s.toLowerCase();
 // isPangram :: String -> Boolean
 const isPangram = compose(isEmpty, reduceAlphabet, toArray, toLowerCase);
 
-class Pangram {
-  constructor(sentence) {
-    // isPangram :: Void -> Boolean
-    this.isPangram = () => isPangram(sentence || '');
-  }
-}
-
-export default Pangram;
+export default (sentence) => ({
+  // isPangram :: Void -> Boolean
+  isPangram: () => isPangram(sentence || '')
+});
