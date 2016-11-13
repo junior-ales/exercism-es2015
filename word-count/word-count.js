e11baed64e21b6ceee1235281950dd892cd66a74
Junior Ales <edilson.ales.jr@gmail.com>
2016-11-13 01:42:05 +0000

    using different regex in word count

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -14,8 +14,7 @@ const shouldConcatArrays = (xs, ys) => xs.length === 0 || head(xs) === head(ys);
 
 // groupWords :: [String] -> [[String]]
 const groupWords = reduce((acc, x) => {
-  let _xs = [x.toLowerCase()];
-  return shouldConcatArrays(last(acc), _xs) ? concatLast(acc, _xs) : acc.concat([_xs]);
+  return shouldConcatArrays(last(acc), [x]) ? concatLast(acc, [x]) : acc.concat([[x]]);
 }, [[]]);
 
 // sort :: [String] -> [String]
@@ -24,20 +23,17 @@ const sort = xs => xs.slice(0).sort();
 // groupByWords :: [String] -> [[String]]
 const groupByWords = compose(groupWords, sort);
 
-// splitBySpace :: String -> [String]
-const splitBySpace = s => s.split(' ');
+// splitBySeparator :: String -> [String]
+const splitBySeparator = s => s.split(/\s+/);
+
+// toLowerCase :: String -> String
+const toLowerCase = s => s.toLowerCase();
 
 // trim :: String -> String
 const trim = s => s.trim();
 
-// replaceForSpace :: [RegExp] -> String -> String
-const replaceForSpace = curry((seps, s) => seps.reduce((_s, sp) => _s.replace(sp, ' '), s));
-
-// separators :: [RegExp]
-const separators = [/( {2,})/g , /\n+/g, /\t+/g];
-
 // toWordsArray :: String -> [String]
-const toWordsArray = compose(splitBySpace, trim, replaceForSpace(separators));
+const toWordsArray = compose(splitBySeparator, toLowerCase, trim);
 
 class Words {
   // count :: String -> { String: Number }
